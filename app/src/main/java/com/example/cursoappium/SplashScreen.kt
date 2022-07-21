058740e0f95e509f555125496b752015311dbfb0
clarabez <clarinhab@gmail.com>
2022-07-21 19:51:14 -0300

    add splashScreen class

---
+++ app/src/main/java/com/example/cursoappium/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.cursoappium
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    private var TIME_OUT:Long = 5000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        loadSplashScreen()
+    }
+    private fun loadSplashScreen(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_OUT)
+    }
+}
